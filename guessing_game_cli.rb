5ef6d50bbb264de01fd4a4bf1fb7f4d5f6974951
Simon Tucker <sjtucker28@gmail.com>
2020-01-03 17:40:18 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,7 +3,9 @@ def guessing_CLI
   num = rand(6)+1
   user_input = gets.chomp.to_s
   if user_input == num 
-    return ""
+    return "You guessed the correct number!"
+  elsif user_input !== num 
+    return 
   
   
   
