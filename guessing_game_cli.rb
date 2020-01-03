6730050e73301987e5bdcfda9824e6a21d475cda
Simon Tucker <sjtucker28@gmail.com>
2020-01-03 18:02:38 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -5,9 +5,8 @@ def run_guessing_game
   
   if user_input == "exit"
     puts "Goodbye!"
-  elsif user_input == num
+  else user_input == num
   puts "You guessed the correct number!"
-  else 
-    puts "Sorry! the computer guessed 6."
+  
   end
 end
