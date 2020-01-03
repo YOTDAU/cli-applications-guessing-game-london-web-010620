90a1631a21076034d83cb359163e03a5ab359c67
Simon Tucker <sjtucker28@gmail.com>
2020-01-03 17:36:09 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,3 +1,13 @@
 # Code your solution here!
 def guessing_CLI
   
+  
+  
+  
+  
+  
+  context 'user inputs' do
+    it "responds to 'exit'" do
+      expect(self).to receive(:gets).and_return('exit')
+      expect { run_guessing_game }.to output(/Goodbye!/).to_stdout
+    end
